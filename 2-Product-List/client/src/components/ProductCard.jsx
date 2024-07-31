15a81d6daf820afaa7ac0a1cb217a857cf9a352a
niluse <niluse02@gmail.com>
2024-07-31 14:01:02 +0300

    feat: product title

--- 2-Product-List/client/src/components/ProductCard.jsx
+++ 2-Product-List/client/src/components/ProductCard.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const ProductCard = ({ id, image, price, title }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleTitleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
     <div className="product-card">
-        <img src={image} alt={title} />
-        <div className="product-price">
-          <p>{price} $</p>
-        </div>
-        <div className="product-title">
-          <h2>{title}</h2>
-        </div>
+      <img src={image} alt={title} />
+      <div className="product-price">
+        <p>{price} $</p>
+      </div>
+      <div 
+        className={`product-title ${isClicked ? 'expanded' : ''}`}
+        onClick={handleTitleClick}
+        title={title}
+      >
+        <h2>{title}</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
