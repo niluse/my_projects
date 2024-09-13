c69acfb6d83baea6ca5afee59f5d4de52735bc9a
nilufer <niluse02@gmail.com>
2024-09-13 14:40:47 +0300

    fix: yarn version and css access

--- FullStackTemplate/client/src/App.js
+++ FullStackTemplate/client/src/App.js
@@ -1,10 +1,7 @@
-import './App.css';
-import Homepage from './pages/Homepage';
+import Homepage from "./pages/Homepage";
 
 function App() {
-  return (
-    <Homepage/>
-  );
+  return <Homepage />;
 }
 
 export default App;
