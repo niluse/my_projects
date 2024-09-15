1a8bb4dd5c91c94cedac679bf20b618c764e6602
nilufer <niluse02@gmail.com>
2024-09-15 12:09:26 +0300

    feat: homepage

--- 3-NBA-Legends/client/src/pages/Homepage.jsx
+++ 3-NBA-Legends/client/src/pages/Homepage.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import CardContainer from "../components/CardContainer";
 
 const Homepage = () => {
-  return (
-    <div>Homepage</div>
-  )
-}
+  const [backendData, setBackendData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-export default Homepage
+  const url = "http://127.0.0.1:8000";
+
+  const fetchPlayers = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result && Array.isArray(data.result.rows)) {
+          setBackendData(data.result.rows);
+        } else {
+          console.log("unexpected data format: ", data);
+        }
+      })
+      .catch((error) => {
+        console.error("there was a problem with the fetch operation: ", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchPlayers();
+  }, []);
+
+  console.log(backendData);
+  return (
+    <div>
+      <Header />
+      <CardContainer />
+    </div>
+  );
+};
+
+export default Homepage;
