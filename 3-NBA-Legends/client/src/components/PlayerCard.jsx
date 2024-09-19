2da1f692507f335a634a8a6f379d266331ddad59
nilufer <niluse02@gmail.com>
2024-09-19 12:16:10 +0300

    feat: statistics style

--- 3-NBA-Legends/client/src/components/PlayerCard.jsx
+++ 3-NBA-Legends/client/src/components/PlayerCard.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PlayerCard = ({ name, img, statistic }) => {
   const { points, rebounds, assists, allStar } = statistic;
-  console.log(statistic);
+  const [clicked, setCliked] = useState(false);
+
+  const handleClick = () => {
+    setCliked(!clicked);
+  };
 
   return (
-    <div>
-      <div>
+    <div className="player-card">
+      <div
+        className={clicked ? "image invisible" : "image visible"}
+        onClick={handleClick}>
         <img src={img} alt={`${name}'s image`} />
         <h2>{name}</h2>
       </div>
-      <div>
-        <p>Points: {points}</p>
-        <p>Rebounds: {rebounds}</p>
-        <p>Assists: {assists}</p>
-        <p>All-Star Appearances: {allStar}</p>
+      <div className={clicked ? "visible" : "invisible"} onClick={handleClick}>
+        <p>🏀 Points: {points}</p>
+        <p>🏀 Rebounds: {rebounds}</p>
+        <p>🏀 Assists: {assists}</p>
+        <p>🏀 All-Star Appearances: {allStar}</p>
+        {console.log(clicked)}
       </div>
     </div>
   );
